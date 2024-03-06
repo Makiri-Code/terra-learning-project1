f9c3ef941ba726b987734f8eca12897a5b008c66
unknown <manuel.makiri@gmail.com>
2024-03-06 17:46:32 +0100

    added bootstrap

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const navBar = document.getElementById('nav-bar');
-const navIcon = document.getElementById('nav-icon');
+const openFaBar = document.getElementById("fa-bar");
+const closeFaBar = document.getElementById("fa-xmark");
 
-const showHamburger = () => {
-    if( navBar.classList === 'nav-bar-container') {
-        navBar.classList += ' responsive';
-    } else {
-        navBar.classList === 'nav-bar-container';
-    }
-    console.log('button is working')
-};
+closeFaBar.hidden = "false"
 
-navIcon.addEventListener('click', showHamburger)
+const openSideBar = () => {
+    closeFaBar.hidden = "true"
+    console.log('side bar is working')
+}
+openFaBar.addEventListener('click', openSideBar)
